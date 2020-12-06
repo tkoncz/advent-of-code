bb59d97abeba493fe8c755b4f64fb2e9175eb6af
tkoncz <tamas.koncz@emarsys.com>
2020-12-06 14:14:00 +0100

    :sparkles: add solution for day 6

---
+++ scripts/day_6_custom_customs.py
@@ -0,0 +1,37 @@
+with open("inputs/day_6_input.txt", "r") as input:
+    lines = list(input.read().rstrip().split('\n\n'))
+
+groups = [l.split('\n') for l in lines]
+
+# part 1
+sum_unique_letters = 0
+
+for group in groups:
+    if len(group) == 1:
+        unique_letters = [letter for letter in group[0]]
+    else:
+        unique_letters = []
+        for person in group:
+            person_letters = [letter for letter in person]
+            new_letters = list(set(person_letters) - set(unique_letters))
+            unique_letters.extend(new_letters)
+
+    sum_unique_letters += len(unique_letters)
+
+print(sum_unique_letters)
+
+# part 2
+sum_common_letters = 0
+
+for group in groups:
+    if len(group) == 1:
+        common_letters = [letter for letter in group[0]]
+    else:
+        common_letters = [letter for letter in group[0]]
+        for person in group[1:]:
+            person_letters = [letter for letter in person]
+            common_letters = list(set(person_letters) & set(common_letters))
+
+    sum_common_letters += len(common_letters)
+
+print(sum_common_letters)
