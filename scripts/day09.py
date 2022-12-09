0fb0f0459d6c6465a5d275bbe3b2e00f203c4929
tamaskoncz-hs <tamas.koncz.con@hotstar.com>
2022-12-09 11:42:04 +0100

    ♻️ refactor day 9

--- scripts/day09.py
+++ scripts/day09.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def convert_motion_to_steps(dir, size):
+
+def convert_motion_to_steps(dir: str, size: int) -> list:
     steps = {
         'R': [0,  1],
         'D': [-1, 0],
@@ -10,52 +11,40 @@ def convert_motion_to_steps(dir, size):
 
     return [steps[dir] for i in range(size)]
 
+
+def simulate_tail_path(tail_length: int) -> list:
+    head_pos = np.array([0, 0])
+    tail_positions = [np.array([0, 0]) for i in range(tail_length)]
+    tail_pos_visited = [(0, 0)]
+
+    for head_step in steps:
+        head_pos += head_step
+
+        for i in range(tail_length):
+            if i == 0:
+                pos_diff = head_pos - tail_positions[0]
+            else:
+                pos_diff = tail_positions[i-1] - tail_positions[i]
+        
+            if max(abs(pos_diff)) == 2:
+                tail_step = np.clip(pos_diff, -1, 1)
+                tail_positions[i] += tail_step
+
+        tail_pos_visited.append(tuple(tail_positions[tail_length - 1]))
+
+    return tail_pos_visited
+
+
 with open("inputs/input09.txt", "r") as input:
     steps_nested = [
-        convert_motion_to_steps(line[0], int(line[2:]))
+        convert_motion_to_steps(line[0], int(line[2:])) 
         for line in input.read().rstrip().split('\n')
     ]
 
 steps = [np.array(x) for step in steps_nested for x in step]
 
 # part 1
-head_pos = np.array([0, 0])
-tail_pos = np.array([0, 0])
-tail_pos_visited = [(0, 0)]
-
-for head_step in steps:
-    head_pos += head_step
-    pos_diff = head_pos - tail_pos
-    
-    if max(abs(pos_diff)) == 2:
-        tail_step = np.clip(pos_diff, -1, 1)
-        tail_pos += tail_step
-        tail_pos_visited.append(tuple(tail_pos))
-
-
-print(len(set(tail_pos_visited)))
-
+print(len(set(simulate_tail_path(tail_length=1))))
 
 # part 2
-head_pos = np.array([0, 0])
-tail_positions = [np.array([0, 0]) for i in range(9)]
-tail_pos_visited = [(0, 0)]
-
-for head_step in steps:
-    head_pos += head_step
-
-    for i in range(9):
-        if i == 0:
-            pos_diff = head_pos - tail_positions[0]
-        else:
-            pos_diff = tail_positions[i-1] - tail_positions[i]
-    
-        if max(abs(pos_diff)) == 2:
-            tail_step = np.clip(pos_diff, -1, 1)
-            tail_positions[i] += tail_step
-
-
-    tail_pos_visited.append(tuple(tail_positions[8]))
-
-
-print(len(set(tail_pos_visited)))
+print(len(set(simulate_tail_path(tail_length=9))))
